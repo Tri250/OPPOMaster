46323f7b94ad434461e9dfa2e3dfb8cd9bda4eb3
Tri250 <Tri250@users.noreply.github.com>
2026-06-03 10:39:09 +0000

    feat: Android源码本地打包APP
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,38 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://mirrors.tencent.com/nexus/repository/maven-public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        mavenLocal()
+        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.application") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.android") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+            if (requested.id.id == "com.google.dagger.hilt.android") {
+                useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.kapt") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.plugin.parcelize") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+        }
+    }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://mirrors.tencent.com/nexus/repository/maven-public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        mavenLocal()
+        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
         google()
         mavenCentral()
     }
